33a9157060a5fef24cb2fe23edf083ac14e94663
Heidi Fryzell <heidi37@users.noreply.github.com>
2026-09-24 08:05:23 -0400

    Vans page fetching data from server js

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
 const Vans = () => {
+
+React.useEffect(function() {
+  fetch("/api/vans")
+  .then(res => res.json())
+  .then(data => console.log(data))
+}, [])
+
   return (
     <div>
       <h1>Vans page goes here 🚐</h1>
